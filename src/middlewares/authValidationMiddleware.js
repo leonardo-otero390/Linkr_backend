0bdd88b955eac5c95723367f95f9370605c1d1be
JefersonBatista <jeferson.de.oliveira.batista@gmail.com>
2022-03-23 19:31:38 -0300

    feat: validate authentication

---
+++ src/middlewares/authValidationMiddleware.js
@@ -0,0 +1,26 @@
+import sessionRepository from '../repositories/sessionRepository.js';
+
+export default async function validateAuth(req, res, next) {
+  const { authorization } = req.headers;
+  const token = authorization?.replace('Bearer ', '');
+
+  if (!token) {
+    return res.status(401).send('Você não está autorizado!');
+  }
+
+  try {
+    const sessionResult = await sessionRepository.getSession(token);
+    const [session] = sessionResult;
+
+    if (!session) {
+      return res.status(401).send('Você não está autorizado!');
+    }
+
+    res.locals.userId = session.userId;
+
+    return next();
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send('There was an internal server error');
+  }
+}
