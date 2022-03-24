bcd5c2ee2cee8f9bda76e205eaa92a8f18cd8b6b
ramon oliveira <ramon30012000@gmail.com>
2022-03-24 12:37:32 -0300

    feat: add all login's functionalites

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
-import { createUser } from '../controllers/userController.js';
+import { createUser, authenticateUser } from '../controllers/userController.js';
 import validateSchema from '../middlewares/schemaValidationMiddleware.js';
+import loginSchema from '../schemas/loginSchema.js';
 import userSchema from '../schemas/userSchema.js';
 
 const userRouter = Router();
 
 userRouter.post('/users', validateSchema(userSchema), createUser);
+userRouter.post('/users/authentication', validateSchema(loginSchema), authenticateUser);
 
 export default userRouter;
