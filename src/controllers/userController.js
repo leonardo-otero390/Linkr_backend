eecff865e185a9e1af2deda8efa8eec4057b809b
ramon oliveira <ramon30012000@gmail.com>
2022-03-24 19:11:08 -0300

    add the use of sessions entity

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,4 @@
 import bcrypt from 'bcrypt';
-import { v4 as uuid } from 'uuid';
 
 import userRepository from '../repositories/userRepository.js';
 
@@ -23,44 +22,6 @@ export async function createUser(req, res) {
 
     res.sendStatus(201);
   } catch (error) {
-    console.error(error);
-    res.status(500).send('There was an internal server error');
-  }
-}
-
-export async function authenticateUser(req, res) {
-  const { email, password } = req.body;
-
-  try {
-    const existingUserResult = await userRepository.getUserByEmail(email);
-
-    if (existingUserResult.rowCount === 0) {
-      return res.status(401).send('The email or password entered is incorrect')
-    }
-    
-    const [user] = existingUserResult.rows;
-    const matchesHashedPassword = bcrypt.compareSync(password, user.password);
-
-    if (!matchesHashedPassword) {
-      return res.status(401).send('The email or password entered is incorrect')
-    }
-    
-    const sessionData = {
-      userId: user.id,
-      token: uuid()
-    };
-
-    await userRepository.createSession(sessionData);
-
-    const auth = {
-      userName: user.name,
-      userPicture: user.pictureUrl,
-      token: sessionData.token
-    }
-
-    res.status(200).send(auth);
-  } catch (error) {
-    console.log(error);
     res.status(500).send('There was an internal server error');
   }
 }
