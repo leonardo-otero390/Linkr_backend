f971bf7d7bd566fbb5eea5eebff73c719e009bcd
Antonio <antonio.garciamn@gmail.com>
2022-03-25 14:18:38 -0300

    feat/getPostsId incomplite

--- src/routers/postsRouter.js
+++ src/routers/postsRouter.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { GetPosts } from '../controllers/postControler.js';
+import { GetPosts, getPostsId } from '../controllers/postControler.js';
 import validateAuth from '../middlewares/authValidationMiddleware.js';
 
 const postsRouter = Router();
-postsRouter.use('/posts', validateAuth, GetPosts);
+postsRouter.get('/posts', validateAuth, GetPosts);
+postsRouter.get('/posts/:id', getPostsId);
 
 export default postsRouter;
