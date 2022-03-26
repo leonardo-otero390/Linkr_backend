0ca42f2ab14a9b5546beddf13719d6f8b052af70
ramon oliveira <ramon30012000@gmail.com>
2022-03-26 20:01:03 -0300

    fixing user without any hashtags error

--- src/controllers/hashtagController.js
+++ src/controllers/hashtagController.js
@@ -3,7 +3,7 @@ import * as hashtagRepository from '../repositories/hashtagRepository.js';
 export async function getTrending(req, res) {
   try {
     const trendingHashtags = await hashtagRepository.listTopHashtags(10);
-    if (!trendingHashtags) return res.status(204).send('No hashtags found');
+    if (!trendingHashtags) return res.status(200).send([]);
     return res.status(200).send(trendingHashtags);
   } catch (error) {
     return res.sendStatus(500);
