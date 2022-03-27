70d92291eb3c13a48348d6ae3fcb462e12061596
JefersonBatista <jeferson.de.oliveira.batista@gmail.com>
2022-03-27 16:36:46 -0300

    fix: put back auth validation in post routes

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -48,6 +48,7 @@ export async function create(req, res) {
     delete user.password;
     return res.status(201).send({ post, user, like: [] });
   } catch (error) {
+    console.error(error);
     return res.sendStatus(500);
   }
 }
