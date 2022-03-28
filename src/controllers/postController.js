b76ee75eb0c2522939b80dfe4e2342fa746983fe
Antonio <antonio.garciamn@gmail.com>
2022-03-28 13:51:53 -0300

    pre merge

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -56,7 +56,6 @@ export async function create(req, res) {
     delete user.password;
     return res.status(201).send({ post, user, like: [] });
   } catch (error) {
-    console.error(error);
     return res.sendStatus(500);
   }
 }
