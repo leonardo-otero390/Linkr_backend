175cce36aa6237edee0944abe9a8cfa87e0764ba
Antonio <antonio.garciamn@gmail.com>
2022-03-28 10:55:42 -0300

    pre merge

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 
 import { createUser, getUserByName } from '../controllers/userController.js';
+import validateAuth from '../middlewares/authValidationMiddleware.js';
 import validateSchema from '../middlewares/schemaValidationMiddleware.js';
 import userSchema from '../schemas/userSchema.js';
 
 const userRouter = Router();
 
 userRouter.post('/users', validateSchema(userSchema), createUser);
-userRouter.post('/users/name', getUserByName);
+userRouter.post('/users/name', validateAuth, getUserByName);
 
 export default userRouter;
