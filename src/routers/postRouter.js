aa2bff3596c412bf7f102f218c6e1e611770b560
Leonardo Otero <leonardo.otero390@gmail.com>
2022-03-28 11:14:19 -0400

    fix: token validation create post

--- src/routers/postRouter.js
+++ src/routers/postRouter.js
@@ -3,6 +3,7 @@ import validateSchema from '../middlewares/schemaValidationMiddleware.js';
 import * as postSchemas from '../schemas/postSchemas.js';
 import * as postController from '../controllers/postController.js';
 import { getPosts, getPostsById } from '../controllers/postController.js';
+import validateAuth from '../middlewares/authValidationMiddleware.js';
 
 const PostsRoute = new Router();
 
@@ -10,6 +11,7 @@ PostsRoute.get('/posts', getPosts);
 PostsRoute.get('/posts/:id', getPostsById);
 PostsRoute.post(
   '/posts',
+  validateAuth,
   validateSchema(postSchemas.newPost),
   postController.create
 );
