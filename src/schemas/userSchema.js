6a28971b04933608557ed44fc58544be591715e5
JefersonBatista <jeferson.de.oliveira.batista@gmail.com>
2022-03-29 15:08:31 -0300

    Add restriction username with a minimum of 3 chars

--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -3,7 +3,7 @@ import joi from 'joi';
 const userSchema = joi.object({
   email: joi.string().email().required(),
   password: joi.string().required(),
-  name: joi.string().required(),
+  name: joi.string().min(3).required(),
   pictureUrl: joi.string().uri().required(),
 });
 
