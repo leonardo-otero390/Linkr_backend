29d3e95909a73a74b921f273f20c52ac0f850d97
Leonardo Otero <leonardo.otero390@gmail.com>
2022-03-30 14:08:13 -0400

    fix: unfollow route

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -4,6 +4,7 @@ import {
   createUser,
   getUserByName,
   follow,
+  unfollow,
 } from '../controllers/userController.js';
 import validateSchema from '../middlewares/schemaValidationMiddleware.js';
 import userSchema from '../schemas/userSchema.js';
@@ -14,5 +15,6 @@ const userRouter = Router();
 userRouter.post('/users', validateSchema(userSchema), createUser);
 userRouter.get('/users', getUserByName);
 userRouter.post('/users/:id/follow', validateAuth, follow);
+userRouter.delete('/users/:id/follow', validateAuth, unfollow);
 
 export default userRouter;
